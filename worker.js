77f5dbb673bb807c6001c6e45f65ed3169a92347
svylabs <sg@svylabs.com>
2023-09-08 18:43:35 +0200

    Trying to see if stringify and non stringify works

--- worker.js
+++ worker.js
@@ -13,18 +13,25 @@ async function init_wasm_in_worker() {
     // Create a new object of the `NumberEval` struct.
     // Set callback to handle messages passed to the worker.
     self.onmessage = async event => {
-        const input = JSON.parse(event.data);
+        let input = {};
+        try {
+           input = JSON.parse(event.data);
+        } catch (e) {
+            input = event.data;
+        }
+
         // By using methods of a struct as reaction to messages passed to the
         // worker, we can preserve our state between messages.
         const num_choices = input.num_choices;
         const choices = input.choices;
         const poll_pub_key = input.poll_pub_key;
+        const platform = input.platform;
         if (choices.length == 1) {
             const result = vote_single(choices[0], num_choices, poll_pub_key);
-            self.postMessage(JSON.stringify({poll_id: input.poll_id, num_choices, choices, poll_pub_key, result}));
+            self.postMessage(JSON.stringify({poll_id: input.poll_id, num_choices, choices, poll_pub_key, result, platform}));
         } else {
             const result = vote_multi(choices, num_choices, poll_pub_key);
-            self.postMessage(JSON.stringify({poll_id: input.poll_id, num_choices, choices, poll_pub_key, result}));
+            self.postMessage(JSON.stringify({poll_id: input.poll_id, num_choices, choices, poll_pub_key, result, platform}));
         }
 
         // Send response back to be handled by callback in main thread.
